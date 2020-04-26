9c06aca0d297ffa69ecc9a56d9597e8f87432e32
johannaraden <51335476+johannaraden@users.noreply.github.com>
2020-04-26 12:39:50 +0200

    created files, reducer, components, pages and library

--- code/src/App.js
+++ code/src/App.js
@@ -1,29 +1,24 @@
 import React from "react";
-import { BarcodeScanner } from "components/BarcodeScanner";
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { reducer } from '/reducers/reducer'
+import { store } from 'reducers/reducer' 
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 
-const onDetected = (code) => {
-  //console.log(`Code: ${code}`);
-  fetch(`https://world.openfoodfacts.org/api/v0/product/${code}.json`)
-    .then((data) => data.json())
-    .then((json) => {
-      console.log(json);
-    });
-};
+const reducer = combineReducers({ store:reducer })
+const store = configureStore({ reducer })
+
 
 export const App = () => {
+
   return (
-    <div>
-      <label>
-        {" "}
-        Test codes here:{" "}
-        <input type="text" onChange={(e) => onDetected(e.target.value)}></input>
-      </label>
-      <p>
-        {" "}
-        Use the field above to test barcodes manually and keep an eye on your
-        console in the browser. i.e. Type 7311070347272 - Pågen Gifflar. Yum
-      </p>
-      <BarcodeScanner onDetected={onDetected}></BarcodeScanner>
-    </div>
+    <Provider>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact></Route>
+          <Route path="/productinput" exact></Route>
+          <Route path="/productinfo" exact></Route>
+        </Switch>
+      </BrowserRouter>
+    </Provider>
   );
 };
---
+++ code/src/reducers/reducer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = { products, vegan: false, vegetarian: false }
+
+export const store = createSlice({
+    name: 'store',
+    initialState,
+    reducers: {
+        // choose method
+        // input product
+        // get info
+        // start over 
+
+    }
+ }
+)
+ 
